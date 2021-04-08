9328d94239a2b026bee29ebf92172e52006195c6
Vargas George Alexandru <george.vargas@e-uvt.ro>
2021-04-08 09:00:53 +0300

    SparkSession cu eroare java.io.IOException: Could not locate executable null\bin\winutils.exe in the Hadoop binaries.

--- ProiectIBM/src/main/java/Main.java
+++ ProiectIBM/src/main/java/Main.java
@@ -1,9 +1,15 @@
 
-
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.*;
 public class Main {
     public static void main(String []args)
     {
-        System.out.println("AnotherTest");
+        SparkSession app= SparkSession.builder()
+                                        .appName("BigData")
+                                            .config("spark.master","local").getOrCreate();
+
+        System.out.println("Hello");
+        app.stop();
 
 }
 
