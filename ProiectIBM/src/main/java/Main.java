6263771fa2e700be2ce912503fb0336058a48aa5
Vargas George Alexandru <george.vargas@e-uvt.ro>
2021-04-13 10:01:48 +0300

    Dataset cu schema.

--- ProiectIBM/src/main/java/Main.java
+++ ProiectIBM/src/main/java/Main.java
@@ -2,18 +2,31 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class Main {
+
     public static void main(String []args)
     {
         System.setProperty("hadoop.home.dir", System.getProperty("user.home"));
         SparkSession spark=SparkSession.builder()
                                         .appName("BigData")
                                             .config("spark.master","local[*]").getOrCreate();
+        StructType schema = DataTypes.createStructType(new StructField[] {
+                DataTypes.createStructField("School unit name",  DataTypes.StringType, false),
+                DataTypes.createStructField("Elementary school cases", DataTypes.IntegerType, true),
+                DataTypes.createStructField("Middle school cases", DataTypes.IntegerType, true),
+                DataTypes.createStructField("High school cases", DataTypes.IntegerType, true),
+                DataTypes.createStructField("Gender", DataTypes.StringType, true),
+                DataTypes.createStructField("Reporting date",DataTypes.DateType,true)
+        });
 
        Dataset<Row> data= spark.read().format("csv")
-                                    .option("header","true")
+                                    .option("header","true").schema(schema)
                                      .load("ProiectIBM/data.csv");
+
        data.printSchema();
 
        data.show(50);
