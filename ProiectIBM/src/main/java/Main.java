940baf3ba7a429d58a17bfb9ce1e2bcb7181408d
Vargas George Alexandru <george.vargas@e-uvt.ro>
2021-04-13 09:33:40 +0300

    Dataset incomplet.

--- ProiectIBM/src/main/java/Main.java
+++ ProiectIBM/src/main/java/Main.java
@@ -1,4 +1,6 @@
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class Main {
@@ -8,6 +10,13 @@ public class Main {
         SparkSession spark=SparkSession.builder()
                                         .appName("BigData")
                                             .config("spark.master","local[*]").getOrCreate();
+
+       Dataset<Row> data= spark.read().format("csv")
+                                    .option("header","true")
+                                     .load("ProiectIBM/data.csv");
+       data.printSchema();
+
+       data.show(50);
 
         System.out.println("Hello");
         System.out.println(System.getProperty("user.home"));
