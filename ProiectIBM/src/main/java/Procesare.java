f02fc2831ef7022984540afd75035f28fa5fce92
Vargas George Alexandru <george.vargas@e-uvt.ro>
2021-04-27 11:09:28 +0300

    procesare neterminata

---
+++ ProiectIBM/src/main/java/Procesare.java
@@ -0,0 +1,63 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import static org.apache.spark.sql.functions.*;
+
+public class Procesare {
+    private final SparkSession s;
+    private final Dataset<Row> data;
+    private final static String elementaryCases="Elementary school cases";
+    private final static String middleCases="Middle school cases";
+    private final static String highCases="High school cases";
+    private final static StructType schema = DataTypes.createStructType(new StructField[] {
+            DataTypes.createStructField("School unit name",  DataTypes.StringType, false),
+            DataTypes.createStructField("Elementary school cases", DataTypes.IntegerType, true),
+            DataTypes.createStructField("Middle school cases", DataTypes.IntegerType, true),
+            DataTypes.createStructField("High school cases", DataTypes.IntegerType, true),
+            DataTypes.createStructField("Gender", DataTypes.StringType, true),
+            DataTypes.createStructField("Reporting date",DataTypes.DateType,true)
+    });
+    public Procesare(SparkSession s)
+    {
+        this.s=s;
+        data=this.s.read().format("csv")
+                .option("header","true").schema(schema)
+                .load("ProiectIBM/data.csv");
+    }
+    public void afisare()
+    {
+        data.printSchema();
+        data.show(50);
+    }public Dataset<Row> tempDF(String numeCol)
+    {
+        return data.groupBy(col("School unit name"),col("Gender"))
+                .agg(sum(numeCol));
+    }
+    public Dataset<Row> dfFinal()
+    {
+        Dataset<Row> df1=tempDF(elementaryCases);
+        Dataset<Row> df2=tempDF(middleCases);
+        Dataset<Row> df3=tempDF(highCases);
+        df1.show();
+        df2.show();
+        df3.show();
+        return df1;
+    }
+    public Dataset<Row> dfFinal2()
+    {
+
+        return data.groupBy(col("School unit name"),col("Gender")).agg(sum("Elementary school cases")
+       ,sum("Middle school cases"),sum("High school cases"));
+
+
+    }
+    public SparkSession getS()
+    {
+        return this.s;
+    }
+
+}
