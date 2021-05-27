3321613da4546c9412fa0a2df95032ecab295423
Vargas George Alexandru <george.vargas@e-uvt.ro>
2021-05-27 18:32:30 +0300

    Mici modificari.

---
+++ ProiectIBM/src/main/java/app/BuildSs.java
@@ -0,0 +1,54 @@
+package app;
+
+import org.apache.spark.sql.SparkSession;
+
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.api.java.UDF2;
+import org.apache.spark.sql.types.DecimalType;
+
+
+import java.math.BigDecimal;
+
+
+public class BuildSs  {
+
+
+   protected SparkSession spark;
+    public  BuildSs()
+    {
+
+
+        spark=SparkSession.builder()
+                .appName("BigData")
+                .config("spark.master","local[*]").getOrCreate();
+
+
+        UDF1<Double, BigDecimal> twoDecimals=new UDF1<Double, BigDecimal>() {
+            @Override
+            public BigDecimal call(Double aDouble) throws Exception {
+
+
+
+                return new BigDecimal(aDouble.toString());
+            }
+        };
+        UDF2<BigDecimal,BigDecimal,BigDecimal> newAvg =new UDF2<BigDecimal, BigDecimal, BigDecimal>() {
+            @Override
+            public BigDecimal call(BigDecimal bigDecimal, BigDecimal bigDecimal2) throws Exception {
+                if(bigDecimal==null)return bigDecimal2;
+                if(bigDecimal2==null)return bigDecimal;
+                return new BigDecimal((bigDecimal.doubleValue()+bigDecimal2.doubleValue())/2);
+            }
+        };
+        spark.udf().register("twoDecimals",twoDecimals,new DecimalType(38,2));
+        spark.udf().register("newAvg",newAvg,new DecimalType(38,2));
+
+
+    }
+    public SparkSession getSpark()
+    {
+        return spark;
+    }
+
+}
+
---
+++ ProiectIBM/src/main/java/app/Main.java
@@ -0,0 +1,26 @@
+package app;
+
+public class Main {
+
+    public static void main(String []args)
+    {
+        System.setProperty("hadoop.home.dir", System.getProperty("user.home"));
+
+
+        Procesare p=new Procesare();
+
+
+        ToolDB db=new ToolDB();
+
+
+
+        p.procesare().show(100);
+
+        db.write(p.procesare());
+
+        p.getSpark().stop();
+
+
+    }
+
+}
